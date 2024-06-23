e1410e8decb43a228dbca41625d358bbd6c2a52f
Brian <bvw20002010@gmail.com>
2024-06-23 18:51:11 -0400

    remove slash commands

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ from tour_finder import tour_stats
 @bot.event
 async def on_ready() -> None:
     """Message to send in log if online and ready."""
-    await bot.tree.copy_global_to(guild=discord.object("363116664558059521"))
-    await bot.tree.copy_global_to(guild=discord.object("735698850802565171"))
-    await bot.tree.sync(guild=discord.object("363116664558059521"))
-    await bot.tree.sync(guild=discord.object("735698850802565171"))
+    # await bot.tree.copy_global_to(guild=discord.object("363116664558059521"))
+    # await bot.tree.copy_global_to(guild=discord.object("735698850802565171"))
+    # await bot.tree.sync(guild=discord.object("363116664558059521"))
+    # await bot.tree.sync(guild=discord.object("735698850802565171"))
 
     print(f"Bot online and logged in as {bot.user}")
 
