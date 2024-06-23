58b3049693c59664894037784d425146b50096a8
Brian <bvw20002010@gmail.com>
2024-06-23 18:08:35 -0400

    add kyle fix

--- song_finder.py
+++ song_finder.py
@@ -23,7 +23,7 @@ def song_name_fix(song: str) -> str:
 @bot.command(name="song")
 async def song_finder(ctx: commands.Context, *, args: str = "") -> None:
     """Get info on inputted song."""
-    if ctx.author.id == 172307315549143040:
+    if ctx.author.id in [172307315549143040, 691399990752313394]:
         await ctx.send("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     else:
         args = (
